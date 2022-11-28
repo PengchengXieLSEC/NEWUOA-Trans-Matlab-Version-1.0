d900c15a55672387f9cd373a2ad14ec7cc0b96ec
PengchengXieLSEC <xpc@lsec.cc.ac.cn>
2022-11-28 10:04:18 +0800

    Add files via upload

---
+++ calfun.m
@@ -0,0 +1,17 @@
+%NEWUOA-Trans-Matlab-Version-1.0 
+%Copyright: Pengcheng Xie 
+%Connect: xpc@lsec.cc.ac.cn
+
+function [F] = calfun (PROBLEM, N, X)
+  % F = 0.0
+  % [G, ~] = evalfun(PROBLEM, N, X);
+
+  F = 0.0e0;
+  % for I = 1:N
+  %   F = F + (X(I))^2;
+  % end
+  % F = (F - 1)^2;
+  % print *, 100.0D0 * F, G, 100.0D0 * F + G
+
+  % F = 100.0D0 * F + G
+end
